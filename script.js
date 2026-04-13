b5cc5b511c79bea7229f81f8f6a53c7e834d36d8
Rafsun <mdrafsunjani76@gmail.com>
2026-04-13 16:00:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMsg() {
+  alert("Hello Rafsan! Your portfolio is working 🚀");
+}
+
+function showMsg() {
+  alert("Hello Rafsan! Your portfolio is working 🚀");
+}
+
+function sendMsg(event) {
+  event.preventDefault();
+  alert("Message sent successfully ✅");
+}
